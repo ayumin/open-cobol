b8a5f4430c9d75b42b16c465aa43aca6f64cfdff
simrw <simrw@ed166372-6744-4ac0-a67f-bb1ae9efa102>
2013-09-19 23:27:48 +0000

    Changes by Roger While for version 2.0
    General:
    	* MARK - Version 2.0
    	* Move to GPL/LGPL 3
    
    Compiler:
    - adding more content to the message later -
    
    Testsuite:
    	* Gathered all tests in one source directoy
    	* Allow tests to run without ISAM I/O
    	* Added tests for new stuff and bugfixes
    
    Internationalization:
    	* Move to GPL/LGPL 3
    	* Added Spanish translation
    
    Minor changes:
    	* Move to newer development tools
    	* Added usable config.h versions for building with VC 2005-2010
    	* Added RPM spec file

--- branches/open-cobol-2.0/libcob.h
+++ branches/open-cobol-2.0/libcob.h
@@ -1,22 +1,23 @@
 /*
- * Copyright (C) 2002-2009 Keisuke Nishida
- * Copyright (C) 2007-2009 Roger While
- *
- * This library is free software; you can redistribute it and/or
- * modify it under the terms of the GNU Lesser General Public License
- * as published by the Free Software Foundation; either version 2.1,
- * or (at your option) any later version.
- *
- * This library is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU Lesser General Public License for more details.
- *
- * You should have received a copy of the GNU Lesser General Public
- * License along with this library; see the file COPYING.LIB.  If
- * not, write to the Free Software Foundation, 51 Franklin Street, Fifth Floor
- * Boston, MA 02110-1301 USA
- */
+   Copyright (C) 2002,2003,2004,2005,2006,2007 Keisuke Nishida
+   Copyright (C) 2007-2012 Roger While
+
+   This file is part of OpenCOBOL.
+
+   The OpenCOBOL runtime library is free software: you can redistribute it
+   and/or modify it under the terms of the GNU Lesser General Public License
+   as published by the Free Software Foundation, either version 3 of the
+   License, or (at your option) any later version.
+
+   OpenCOBOL is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+   GNU Lesser General Public License for more details.
+
+   You should have received a copy of the GNU Lesser General Public License
+   along with OpenCOBOL.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
 
 #ifndef COB_LIBCOB_H
 #define COB_LIBCOB_H
@@ -27,17 +28,7 @@
 extern "C" {
 #endif
 
-#include <libcob/byteswap.h>
-#include <libcob/call.h>
 #include <libcob/common.h>
-#include <libcob/fileio.h>
-#include <libcob/move.h>
-#include <libcob/numeric.h>
-#include <libcob/screenio.h>
-#include <libcob/strings.h>
-#include <libcob/termio.h>
-#include <libcob/intrinsic.h>
-#include <libcob/codegen.h>
 
 #ifdef __cplusplus
 }
