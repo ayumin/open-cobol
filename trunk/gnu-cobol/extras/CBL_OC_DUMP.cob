98a4e3498bea4cfdb8e5c6e520a53d24d9ac24df
sf-mensch <sf-mensch@ed166372-6744-4ac0-a67f-bb1ae9efa102>
2013-10-31 22:38:35 +0000

    Documentation: - listing correct years in copyright - add information about config options in config/xyz.conf while removing include of default.conf (every conf file contains all configuration entries now) - testsuite: added keywords for test groups and colors - updated ABOUT-NLS

--- trunk/gnu-cobol/extras/CBL_OC_DUMP.cob
+++ trunk/gnu-cobol/extras/CBL_OC_DUMP.cob
@@ -11,9 +11,9 @@
       *>
       *>  This file is part of GNU Cobol.
       *>
-      *>  The GNU Cobol compiler is free software: you can redistribute
+      *>  The GNU Cobol compiler is free software; you can redistribute
       *>  it and/or modify it under the terms of the GNU General Public
-      *>  License as published by the Free Software Foundation, either
+      *>  License as published by the Free Software Foundation; either
       *>  version 2 of the License, or (at your option) any later
       *>  version.
       *>
@@ -23,8 +23,9 @@
       *>  GNU General Public License for more details.
       *>
       *>  You should have received a copy of the GNU General Public
-      *>  License along with GNU Cobol.
-      *>  If not, see <http://www.gnu.org/licenses/>.
+      *>  License along with GNU Cobol; see the file COPYING.  If not,
+      *>  write to the Free Software Foundation, 51 Franklin Street,
+      *>  Fifth Floor, Boston, MA 02110-1301 USA
 
        IDENTIFICATION   DIVISION.
        PROGRAM-ID.      CBL_OC_DUMP.
