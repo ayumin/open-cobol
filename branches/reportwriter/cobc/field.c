a5a088b4935d7ce73fb221c7d973c637c959f8a4
sf-mensch <sf-mensch@ed166372-6744-4ac0-a67f-bb1ae9efa102>
2014-06-13 13:50:18 +0000

    * Adding test cases for RW (94: LINE PLUS 0/zero currently fails) * regenerated cobc/parser.c */.Makefile.in * correction of file headers

--- branches/reportwriter/cobc/field.c
+++ branches/reportwriter/cobc/field.c
@@ -2,20 +2,20 @@
    Copyright (C) 2001,2002,2003,2004,2005,2006,2007 Keisuke Nishida
    Copyright (C) 2007-2012 Roger While
 
-   This file is part of OpenCOBOL.
+   This file is part of GNU Cobol.
 
-   The OpenCOBOL compiler is free software: you can redistribute it
+   The GNU Cobol compiler is free software: you can redistribute it
    and/or modify it under the terms of the GNU General Public License
    as published by the Free Software Foundation, either version 3 of the
    License, or (at your option) any later version.
 
-   OpenCOBOL is distributed in the hope that it will be useful,
+   GNU Cobol is distributed in the hope that it will be useful,
    but WITHOUT ANY WARRANTY; without even the implied warranty of
    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
    GNU General Public License for more details.
 
    You should have received a copy of the GNU General Public License
-   along with OpenCOBOL.  If not, see <http://www.gnu.org/licenses/>.
+   along with GNU Cobol.  If not, see <http://www.gnu.org/licenses/>.
 */
 
 
