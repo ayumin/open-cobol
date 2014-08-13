0f9136e55fa036339cb6b47c334fb956bdcff0a0
sf-mensch <sf-mensch@ed166372-6744-4ac0-a67f-bb1ae9efa102>
2014-08-13 17:58:37 +0000

    Removed "contrib" as it was moved into an own repository

--- trunk/contrib/tools/cobxref/comp-cobxref.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/sh
-cobc -x -O2 cobxref.cbl get-reserved-lists.cbl
-chmod +x cobxref
-exit
--- trunk/contrib/tools/cobxref/lprr.sh
+++
@@ -1,1 +0,0 @@
-lpr -o 'landscape sides=two-sided-long-edge cpi=12 lpi=8 page-left=18 page-top=48' %1
